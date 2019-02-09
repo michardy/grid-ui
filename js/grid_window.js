edf41656c9992d7c0ed28085f80d615bb4831243
Michael Hardy <98.michael.hardy@gmail.com>
2019-02-09 00:50:55 -0800

    Adding dynamic pane color, tab styles, js namespacing, and progress bars

--- js/grid_window.js
+++ js/grid_window.js
@@ -23,24 +23,61 @@
  * SOFTWARE.
  */
 
-function popover(url) {
-	let pop = document.getElementById('popover');
-	pop.innerHTML = null;
-	let close = document.createElement('button');
-	close.textContent = 'X';
-	close.addEventListener("click", close_popover);
-	close.className += "icon";
-	close.className += " button";
-	pop.appendChild(close);
-	let pop_window = document.createElement('iframe');
-	pop_window.src = url;
-	pop_window.className += "view";
-	pop.appendChild(pop_window);
-	pop.style.display = "block";
-	return false;
-}
+var grid_ui = {
+	close_popover: function() {
+		let pop = document.getElementById('popover');
+		pop.style.display = "none";
+	},
+	popover: function(url) {
+		let pop = document.getElementById('popover');
+		pop.innerHTML = null;
+		let icon_bar = document.createElement('div');
+		icon_bar.className += "icon";
+		let close = document.createElement('button');
+		let link = document.createElement('a');
+		close.textContent = '❌';
+		link.textContent = '🔗';
+		close.addEventListener("click", this.close_popover);
+		link.href = url;
+		close.className += "button";
+		link.className += "button";
+		icon_bar.appendChild(link);
+		icon_bar.appendChild(close);
+		pop.appendChild(icon_bar);
+		let pop_window = document.createElement('iframe');
+		pop_window.src = url;
+		pop_window.className += "view";
+		pop.appendChild(pop_window);
+		pop.style.display = "block";
+		return false;
+	},
+	switch_to: function(name, value) {
+		let crumbs = document.getElementsByClassName('tabcrumb_' + name);
+		for (let crumb of crumbs) {
+			if (crumb.dataset.crumb.split(',').includes(value)) {
+				if (crumb.tagName === "SPAN" || crumb.tagName === "A" || crumb.tagName === "B" || crumb.tagName === "STRONG") {
+					crumb.style.display = "inline";
+				} else {
+					crumb.style.display = "block";
+				}
+			} else {
+				crumb.style.display = "none";
+			}
+		}
+	},
+	tab_change: function(event) {
+		grid_ui.switch_to(event.target.name, event.target.value);
+	},
+	init: function() {
+		/* set states of tabctrls */
+		for (let tabctrl of document.getElementsByClassName('fatradio-button')) {
+			tabctrl.addEventListener("input", this.tab_change);
+			if (tabctrl.checked) {
+				this.switch_to(tabctrl.name, tabctrl.value);
+			}
+		}
+	}
+};
 
-function close_popover() {
-	let pop = document.getElementById('popover');
-	pop.style.display = "none";
-}
+console.log('test');
+grid_ui.init();
